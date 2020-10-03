9264cc3f2773d88420897bf2a2d98f391720dacf
Tiliz Tiadi <tiliztiadi@gmail.com>
2020-10-03 12:24:51 +0800

    change deactivation message

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -150,12 +150,12 @@ class PaymentController extends Controller
                         ->notify(new PaymentStatus($payment));
 
             DB::commit();
-            return response()->json(['message' => 'activation success'], 200);
+            return response()->json(['message' => 'deactivation payment success'], 200);
         } catch (\Exception $e) {
             logger($e->getMessage()); // record error to log
 
             DB::rollBack(); // rollback update data
-            return response()->json(['message' => 'activation failed'], 500);
+            return response()->json(['message' => 'deactivation payment failed'], 500);
         }
 
     }
